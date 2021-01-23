6373faedd84bc3155820017c1cd4feeabccdc764
wujunyi <junyi.wu@joyfish.com>
2021-01-24 00:29:29 +0800

    Pointcut 和 Advice 组合

---
+++ src/main/java/org/springframework/aop/aspectj/AspectJExpressionPointcutAdvisor.java
@@ -0,0 +1,32 @@
+package org.springframework.aop.aspectj;
+
+import org.aopalliance.aop.Advice;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.PointcutAdvisor;
+
+public class AspectJExpressionPointcutAdvisor implements PointcutAdvisor {
+
+    private AspectJExpressionPointcut pointcut;
+    private Advice advice;
+    private String expression;
+
+    public void setExpression(String expression) {
+        this.expression = expression;
+        this.pointcut = new AspectJExpressionPointcut(expression);
+    }
+
+    @Override
+    public Advice getAdvice() {
+        return this.advice;
+    }
+
+    @Override
+    public Pointcut getPointcut() {
+        return pointcut;
+    }
+
+    public void setAdvice(Advice advice) {
+        this.advice = advice;
+    }
+}
